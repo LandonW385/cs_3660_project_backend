2d71c82ac99d5166935e9a407d1803d9a9139fce
Landon Whitney <whiteboimcflyttv@gmail.com>
2025-04-30 12:15:03 -0600

    Backend updates

--- config.py
+++ config.py
@@ -6,6 +6,11 @@ class Settings(BaseSettings):
     app_env: str
     allow_origins: list[AnyHttpUrl] 
     api_gateway_token: str
+    secret_key: str
+    algorithm: str
+    client_id: str
+    client_secret: str
+
 
     class Config:
         env_file = ".env"
